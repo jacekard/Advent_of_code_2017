fb2a4653f862d04ae601369e2704f6a925a9bdf9
jacekard <jacekard@gmail.com>
2017-12-02 19:08:30 +0100

    Small fixes of comments

--- Day 1/Sum_of_digits/Sum_of_digits/Program.cs
+++ Day 1/Sum_of_digits/Sum_of_digits/Program.cs
@@ -22,14 +22,14 @@ namespace Sum_of_digits {
                 }
             }
 
-            //excercise 1
+            //exercise 1
             for (int i = 0; i < list.Count; i++) {
                 if (list[i] == list[(i + 1) % list.Count])
                     sum += list[i];
             }
             Console.WriteLine(sum);
 
-            //excercise 2
+            //exercise 2
             int average = list.Count / 2;
             for (int i = 0; i < list.Count; i++) {
                 if (list[i] == list[(i + average) % list.Count])
