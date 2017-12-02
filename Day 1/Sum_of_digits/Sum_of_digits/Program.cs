0bb9e618880d547691f13bfc22e34c4ead15519a
jacekard <jacekard@gmail.com>
2017-12-02 18:44:42 +0100

    Adding day 1 code

---
+++ Day 1/Sum_of_digits/Sum_of_digits/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_digits {
+    class Program {
+        static void Main(string[] args) {
+
+            List<int> list = new List<int>();
+            int sum = 0;
+
+            //read input
+            string line = "";
+            while (line != null) {
+                line = Console.ReadLine();
+                if (line == null) break;
+                for (int i = 0; i < line.Length; i++) {
+                    if (int.TryParse(line[i].ToString(), out int number))
+                        list.Add(number);
+                    else break;
+                }
+            }
+
+            //excercise 1
+            for (int i = 0; i < list.Count; i++) {
+                if (list[i] == list[(i + 1) % list.Count])
+                    sum += list[i];
+            }
+            Console.WriteLine(sum);
+
+            //excercise 2
+            int average = list.Count / 2;
+            for (int i = 0; i < list.Count; i++) {
+                if (list[i] == list[(i + average) % list.Count])
+                    sum += list[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
