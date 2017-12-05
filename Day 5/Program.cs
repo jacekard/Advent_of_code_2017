6fe926b8e97d6f5277d632d45c151c05eefd3102
jacekard <jacekard@gmail.com>
2017-12-05 14:59:15 +0100

    Day 5 added

---
+++ Day 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5 {
+    class Program {
+        static void Main(string[] args) {
+
+            List<int> list = new List<int>();
+
+            int counter = 0;
+            string line="";
+            while(line != null) {
+                line = Console.ReadLine();
+                if (line == null)
+                    break;
+                list.Add(int.Parse(line));
+            }
+
+            bool isDone = false;
+            int i = 0;
+            int v = 0;
+            int size = list.Count;
+
+            //exercise 1 & 2 are almost the same so here's just 2nd part
+
+            while(!isDone) {
+                i += list[i];
+                if (list[v] >= 3)
+                    list[v]--;
+                else
+                    list[v]++;
+                v = i;
+                counter++;
+                if (i >= size)
+                    isDone = true;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
