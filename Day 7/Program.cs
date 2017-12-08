3d62e08f0ab18e427a4a7556faf94877988104e6
jacekard <jacekard@gmail.com>
2017-12-08 13:02:52 +0100

    Day 8 added

---
+++ Day 7/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7 {
+    class Program {
+        static void Main(string[] args) {
+            Lista list = new Lista();
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            string line = "";
+            line = Console.ReadLine();
+            while (line != null) {
+
+                string[] elements = line.Split(' ');
+
+                string progName = elements[0];
+                //Prog p = new Prog(elements[0], int.Parse(progName));
+                //list.Add(p);
+
+                if (!map.ContainsKey(progName)) {
+                    map[progName] = 0;
+                }
+
+                for (int j = 3; j < elements.Length; j++) {
+                    if (j != elements.Length - 1) {
+                        string neighborName = elements[j].Substring(0, elements[j].Length - 1);
+                        //p.Neighbors.Add(neighborName);
+                        if (map.ContainsKey(neighborName))
+                            map[neighborName]++;
+                        else
+                            map[neighborName] = 1;
+
+                    }
+                    else {
+                        if (map.ContainsKey(elements[j]))
+                            map[elements[j]]++;
+                        else
+                            map[elements[j]] = 1;
+                    }
+                }
+                line = Console.ReadLine();
+            }
+
+            var dict = map.OrderBy(x => x.Value);
+            Console.WriteLine(dict.First().Key);
+            
+
+        }
+    }
+    class Lista : List<Prog> {
+        public Lista() {
+
+        }
+
+    }
+
+    class Prog {
+        public string Name { get; set; }
+        public Prog Parent { get; set; }
+        public int Weight { get; set; }
+        public List<string> Neighbors;
+        public Prog(string name, int weight) {
+            Name = name;
+            Weight = weight;
+            Neighbors = new List<string>();
+        }
+        //public Prog getNeighbor(string name) {
+        //    foreach(var e in neighbors) {
+        //        if (e.Equals(name)) {
+
+        //            return p;
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ Day 8/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8 {
+    class Program {
+        static void Main(string[] args) {
+
+            string line = "";
+            line = Console.ReadLine();
+
+            while (line != null) {
+                string[] words = line.Split(' ');
+                string Id = words[0];
+                string instruction = words[1];
+                int value = int.Parse(words[2]);
+
+                string[] condition = new string[words.Length-4];
+                for(int i = 0; i+4 < words.Length; i++) {
+                    condition[i] = words[i+4];
+                }
+
+                Var var = new Var(Id);
+                var = Var.List.Add(var);
+                var.evaluateCondition(condition);
+                var.evaluateInstruction(instruction, value);
+
+                line = Console.ReadLine();
+            }
+
+            int result = Var.List.First().Val;
+            foreach(var v in Var.List) {
+                if (v.Val > result)
+                    result = v.Val;
+            }
+            Console.WriteLine(result);
+            Console.WriteLine(Var.highestValueEver);
+
+        }
+    }
+    class Lista : List<Var> {
+        public Var Add(Var obj) {
+            Var var = Var.List.Find(x => x.Id.Equals(obj.Id));
+            if(var == null) {
+                base.Add(obj);
+                return obj;
+            }
+            else {
+                return var;
+            }
+        }
+
+        public Lista() : base() {}
+    }
+
+    class Var {
+        public string Id { get; set; }
+        public int Val { get; set; }
+        public bool canExecute { get; set; }
+        public static Lista List = new Lista();
+        public static int highestValueEver = 0;
+        public Var(string id) {
+            Val = 0;
+            Id = id;
+            canExecute = false;
+        }
+
+        public void evaluateCondition(string[] condition) {
+            string id = condition[0];
+            Var var = new Var(id);
+            var = List.Add(var);
+            string symbol = condition[1];
+            int value = int.Parse(condition[2]);
+            switch(symbol) {
+                case "<":
+                canExecute = var.Val < value ? true : false; 
+                break;
+
+                case ">":
+                canExecute = var.Val > value ? true : false;
+                break;
+
+                case "<=":
+                canExecute = var.Val <= value ? true : false;
+                break;
+
+                case ">=":
+                canExecute = var.Val >= value ? true : false;
+                break;
+
+                case "==":
+                canExecute = var.Val == value ? true : false;
+                break;
+
+                case "!=":
+                canExecute = var.Val != value ? true : false;
+                break;
+            }
+
+        }
+
+        public void evaluateInstruction(string instruction, int value) { 
+            if(!canExecute) return;
+
+            //if (Val > Var.highestValueEver)
+              //  Var.highestValueEver = value;
+
+            switch (instruction) {
+                case "inc":
+                Val += value;
+                break;
+                case "dec":
+                Val -= value;
+                break;
+            }
+
+            if (Val > Var.highestValueEver)
+                Var.highestValueEver = Val;
+
+            canExecute = false;
+        }
+    }
+}
