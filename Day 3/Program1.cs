50a17a51ea71684d2d91435adce6b0deafb21ad5
jacekard <jacekard@gmail.com>
2017-12-11 21:49:12 +0100

    Day 11 added

---
+++ Day 3/Program1.cs
@@ -0,0 +1,67 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Day_3 {
+//    class Program1 {
+//        static void Main(string[] args) {
+//            int n;
+//            n = int.Parse(Console.ReadLine());
+
+//            Point p = new Point();
+
+//            int distance;
+//            int i = 1;
+//            int step = 1;
+//            while (i <= n) {
+//                for (int j = 0; j < step; j++) {
+//                    if (i > n)
+//                        break;
+//                    p.x += 1;
+//                    i += 1;
+//                }
+
+//                for (int j = 0; j < step; j++) {
+//                    if (i > n)
+//                        break;
+//                    p.y += 1;
+//                    i += 1;
+//                }
+
+//                for (int j = 0; j < step + 1; j++) {
+//                    if (i > n)
+//                        break;
+//                    p.x -= 1;
+//                    i += 1;
+//                }
+
+//                for (int j = 0; j < step + 1; j++) {
+//                    if (i > n)
+//                        break;
+//                    p.y -= 1;
+//                    i += 1;
+//                }
+
+//                step += 2;
+//            }
+//        }
+
+//        class Point {
+
+//            public int x { get; set; }
+//            public int y { get; set; }
+
+//            public Point() {
+//                x = 0;
+//                y = 0;
+//            }
+
+//            public override string ToString() {
+//                return x + "/" + y;
+//            }
+
+//        }
+//    }
+//}
--- Day 3/Program2.cs
+++ Day 3/Program2.cs
@@ -10,58 +10,84 @@ namespace Day_3 {
             int n;
             n = int.Parse(Console.ReadLine());
 
-            Point p = new Point();
-
             int distance;
             int i = 1;
             int step = 1;
-            while (i <= n) {
+            int x = 50, y = 50;
+            for (int j = 0; j < 100; j++) {
+                for (int k = 0; k < 100; k++) {
+                    Square.grid[j, k] = new Square();
+                }
+            }
+            Square.grid[x, y].value = 1;
+
+            while (Square.grid[x,y].value < n) {
                 for (int j = 0; j < step; j++) {
-                    if (i > n)
+                    if (Square.grid[x, y].value > n)
                         break;
-                    p.x += 1;
+                    x += 1;
                     i += 1;
+                    Square.grid[x, y].getNeighbors(x, y);
                 }
 
                 for (int j = 0; j < step; j++) {
-                    if (i > n)
+                    if (Square.grid[x, y].value > n)
                         break;
-                    p.y += 1;
+                    y += 1;
                     i += 1;
+                    Square.grid[x, y].getNeighbors(x, y);
                 }
 
                 for (int j = 0; j < step + 1; j++) {
-                    if (i > n)
+                    if (Square.grid[x, y].value > n)
                         break;
-                    p.x -= 1;
+                    x -= 1;
                     i += 1;
+                    Square.grid[x, y].getNeighbors(x, y);
                 }
 
                 for (int j = 0; j < step + 1; j++) {
-                    if (i > n)
+                    if (Square.grid[x, y].value > n)
                         break;
-                    p.y -= 1;
+                    y -= 1;
                     i += 1;
+                    Square.grid[x, y].getNeighbors(x, y);
                 }
 
                 step += 2;
             }
+            Console.WriteLine(Square.grid[x, y].value);
+        }
+    }
+
+    class Point {
+
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Point() {
+            x = 0;
+            y = 0;
         }
 
-        class Point {
+        public override string ToString() {
+            return x + "/" + y;
+        }
+    }
 
-            public int x { get; set; }
-            public int y { get; set; }
-
-            public Point() {
-                x = 0;
-                y = 0;
-            }
-
-            public override string ToString() {
-                return x + "/" + y;
-            }
-
+    class Square {
+        public static Square[,] grid = new Square[100, 100];
+        public Point pos { get; set; }
+        public int value { get; set; }
+        public Square() {
+            pos = new Point();
+            value = 0;
+        }
+        public void getNeighbors(int x, int y) {
+            value = grid[x + 1, y + 1].value + grid[x + 1, y - 1].value +
+                grid[x - 1, y - 1].value + grid[x - 1, y + 1].value +
+                grid[x + 1, y].value + grid[x, y - 1].value +
+                grid[x, y + 1].value + grid[x - 1, y].value;
         }
     }
 }
