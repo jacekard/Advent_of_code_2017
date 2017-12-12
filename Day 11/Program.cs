193f8f33aee5427eccb81feafdffd50610e9e163
jacekard <jacekard@gmail.com>
2017-12-12 18:27:08 +0100

    Day 9 added

---
+++ Day 11/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11 {
+    class Program {
+        static void Main(string[] args) {
+
+            string line = "";
+            Point p = new Point();
+            line = Console.ReadLine();
+            int distance = 0, newDistance = 0;
+            while (line != null) {
+
+                string[] directions = line.Split(',');
+
+                for (int i = 0; i < directions.Length; i++) {
+                    switch (directions[i]) {
+                        case "n":
+                        p.y--;
+                        break;
+
+                        case "s":
+                        p.y++;
+                        break;
+
+                        case "ne":
+                        p.x++;
+                        p.y--;
+                        break;
+
+                        case "nw":
+                        p.x--;
+                        break;
+
+                        case "sw":
+                        p.x--;
+                        p.y++;
+                        break;
+
+                        case "se":
+                        p.x++;
+                        break;
+                    }
+                    
+                    newDistance = Math.Max(Math.Abs(p.x), Math.Abs(p.y));
+                    newDistance = Math.Max(newDistance, Math.Abs(p.x + p.y));
+                    if (newDistance > distance)
+                        distance = newDistance;
+                }
+
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine(distance);
+
+        }
+    }
+
+    class Point {
+        public int x { get; set; }
+        public int y { get; set; }
+        public Point() {
+            x = 0;
+            y = 0;
+        }
+    }
+
+
+}
---
+++ Day 9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9 {
+    class Program {
+        static void Main(string[] args) {
+
+            string line = "";
+            line = Console.ReadLine();
+            int groupSize = 0;
+            int score = 0;
+            int charCount = 0;
+
+            for (int i = 0; i < line.Length; i++) {
+                if (line[i].Equals('{'))
+                    groupSize++;
+                else if (line[i].Equals('}')) {
+                    score += groupSize;
+                    groupSize--;
+                }
+                else if (line[i].Equals('<')) {
+                    while (!line[++i].Equals('>')) {
+                        if (line[i].Equals('!')) {
+                            i++;
+                            charCount--;
+                        }
+                        charCount++;
+                    }
+                }
+                else if (line[i].Equals('!'))
+                    i++;
+                //else if (line[i].Equals(',')) {
+                //    if (groupSize == 1)
+                //        groupSize = 0;
+            }
+            Console.WriteLine(score);
+            Console.WriteLine(charCount);
+
+        }
+
+    }
+}
