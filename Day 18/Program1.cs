bdb8dc81321f10ea0eb191f9756710a70d69e3af
jacekard <jacekard@gmail.com>
2017-12-23 20:51:34 +0100

    Day 18 Part I added'

---
+++ Day 18/Program1.cs
@@ -0,0 +1,115 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Day_18 {
+//    class Program1 {
+//        static void Main(string[] args) {
+//            Dictionary<char, long> regs = new Dictionary<char, long>();
+//            List<Instruction> list = new List<Instruction>();
+
+//            Instruction instr;
+//            long sound = 0;
+//            bool isDone = false;
+
+//            string line = Console.ReadLine();
+//            while (line != null) {
+//                string[] input = line.Split(' ');
+//                var name = input[0];
+
+
+//                if (name.Equals("snd") || name.Equals("rcv")) {
+//                    instr = new Instruction() {
+//                        Name = name,
+//                        Reg = input[1].First()
+//                    };
+//                }
+//                else {
+//                    char reg = input[1].First();
+//                    if (reg >= 'a' && reg <= 'z') {
+//                        if (!regs.ContainsKey(reg))
+//                            regs.Add(reg, 0);
+//                    }
+//                    else reg -= '0';
+//                    instr = new Instruction() {
+//                        Name = name,
+//                        Reg = reg,
+//                    };
+
+//                    if (input[2].First() >= 'a' && input[2].First() <= 'z') {
+//                        instr.RegParam = input[2].First();
+//                        instr.isRegParam = true;
+//                    }
+//                    else {
+//                        instr.Param = int.Parse(input[2]);
+//                        instr.isRegParam = false;
+//                    }
+//                }
+
+//                list.Add(instr);
+//                line = Console.ReadLine();
+//            }
+
+//            for (int i = 0; i < list.Count && !isDone; i++) {
+//                switch (list[i].Name) {
+//                    case "snd":
+//                    sound = regs[list[i].Reg];
+//                    break;
+//                    case "set":
+//                    if (!list[i].isRegParam)
+//                        regs[list[i].Reg] = list[i].Param;
+//                    else
+//                        regs[list[i].Reg] = regs[list[i].RegParam];
+//                    break;
+//                    case "add":
+//                    if (!list[i].isRegParam)
+//                        regs[list[i].Reg] += list[i].Param;
+//                    else
+//                        regs[list[i].Reg] += regs[list[i].RegParam];
+
+//                    break;
+//                    case "mul":
+//                    if (!list[i].isRegParam)
+//                        regs[list[i].Reg] *= list[i].Param;
+//                    else
+//                        regs[list[i].Reg] *= regs[list[i].RegParam];
+//                    break;
+//                    case "mod":
+//                    if (list[i].isRegParam)
+//                        regs[list[i].Reg] %= regs[list[i].RegParam];
+//                    else
+//                        regs[list[i].Reg] %= list[i].Param;
+//                    break;
+//                    case "rcv":
+//                    if (regs[list[i].Reg] != 0) {
+//                        Console.WriteLine(sound);
+//                        isDone = true;
+//                    }
+//                    break;
+//                    case "jgz":
+//                    if (list[i].isRegParam) {
+//                        if (regs[list[i].Reg] > 0)
+//                            i += (int)regs[list[i].RegParam] - 1;
+//                    }
+//                    else {
+//                        if (regs[list[i].Reg] > 0)
+//                            i += (int)list[i].Param - 1;
+//                    }
+//                    break;
+//                    default: break;
+//                }
+//            }
+//        }
+//    }
+
+//    class Instruction {
+//        public string Name { get; set; }
+//        public char Reg { get; set; }
+//        public bool isRegParam { get; set; }
+//        public char RegParam { get; set; }
+//        public long Param { get; set; }
+//        public Instruction() { }
+//    }
+//}
---
+++ Day 20/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20 {
+    class Program {
+        static void Main(string[] args) {
+            string line = Console.ReadLine();
+            List<Particle> list = new List<Particle>();
+            int id = 0;
+            List<Particle> collisions = new List<Particle>();
+
+            while (line != null) {
+                string[] input = line.Split(new char[] { ' ', ',', '=', '>', '<', 'p', 'v', 'a' },
+                    StringSplitOptions.RemoveEmptyEntries);
+                Vector pos = new Vector(int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]));
+                Vector vel = new Vector(int.Parse(input[3]), int.Parse(input[4]), int.Parse(input[5]));
+                Vector acc = new Vector(int.Parse(input[6]), int.Parse(input[7]), int.Parse(input[8]));
+
+                list.Add(new Particle(id, pos, vel, acc));
+                ++id;
+                line = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 1000; i++) {
+                foreach (var p in list) {
+                    p.updateParticle();
+                }
+                for (int j = 0; j < list.Count; j++)  {
+                    collisions = list.FindAll(x => x.Pos.Equals(list[j].Pos));
+                    if (collisions.Count > 1) {
+                        for (int k = 0; k < collisions.Count; k++) {
+                            list.RemoveAt(k);
+                        }
+                    }
+                }
+            }
+
+            //part 1
+            int min = list[0].countDistance();
+            foreach (var p in list) {
+                if (p.countDistance() <= min) {
+                    min = p.distance;
+                    id = p.Id;
+                }
+                Console.WriteLine(p.Id + " " + p.Pos + " | " + p.Vel + " | " + p.Acc);
+
+            }
+
+            //part 1
+            //Console.WriteLine(id);
+            //part 2
+            Console.WriteLine(list.Count);
+
+
+
+        }
+    }
+
+    class Vector {
+        public int x;
+        public int y;
+        public int z;
+        public Vector(int a, int b, int c) {
+            x = a;
+            y = b;
+            z = c;
+        }
+        public void AddVector(Vector v) {
+            x += v.x;
+            y += v.y;
+            z += v.z;
+        }
+        public override string ToString() {
+            return "(" + x + "," + y + "," + z + ")";
+        }
+        override
+        public  bool Equals(object item) {
+            var obj = item as Vector;
+
+            if (x == obj.x && y == obj.y && z == obj.z)
+                return true;
+            else return false;
+        }
+
+    }
+
+    class Particle {
+        public int Id;
+        public Vector Pos;
+        public Vector Vel;
+        public Vector Acc;
+        public int distance;
+        public Particle(int id, Vector pos, Vector vel, Vector acc) {
+            Id = id;
+            Pos = pos;
+            Vel = vel;
+            Acc = acc;
+        }
+        public int countDistance() {
+            distance = Math.Abs(Pos.x) + Math.Abs(Pos.y) + Math.Abs(Pos.z);
+            return distance;
+        }
+        public void updateParticle() {
+            Vel.AddVector(Acc);
+            Pos.AddVector(Vel);
+        }
+    }
+}
